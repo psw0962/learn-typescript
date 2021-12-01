719c9ecd00833a56d6f6f297d9211d07a8745921
Nick <65061325+psw0962@users.noreply.github.com>
2021-12-01 13:22:48 +0900

    typescript-basic

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,36 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
 function startApp() {
-  axios
-    .get(url)
-    .then(function (response) {
-      console.log(response);
-      user = response.data;
-      // TODO: 이름, 이메일, 주소 표시하기
+  // axios
+  //   .get(url)
+  fetchUser()
+    .then((res) => {
+      console.log(res);
+      user = res.data;
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.street;
     })
     .catch(function (error) {
       console.log(error);
